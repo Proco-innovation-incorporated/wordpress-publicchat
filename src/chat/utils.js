4cf9a26c9aca270bef63767845f05031ebc55626
Bborie Park <dustymugs@gmail.com>
2025-09-22 17:40:32 -0700

    log to debug

--- src/chat/utils.js
+++ src/chat/utils.js
@@ -20,7 +20,7 @@ const md = markdownit({
 // Pass in options if we want to transform elements or add classes
 export function mdToHtml(commonmark, options = {linksNewTab: true}) {
   if (!commonmark) return '<div class="markdown"></div>';
-  console.log("commonmark-request", commonmark)
+  console.debug("commonmark-request", commonmark)
   
   // md -> html
   const html = md.render(commonmark)
